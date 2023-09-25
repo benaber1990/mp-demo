28e8087d4eb5a86e39f441d079ceebc261753f77
benaber1990 <benaber1990@gmail.com>
2023-09-25 14:29:11 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-phase-gatsb`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
